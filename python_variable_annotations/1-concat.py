7b326db22d230060d76a31920bfeb56cd654c238
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2024-09-04 16:24:23 +0200

    feat

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-"""
-    1. Basic annotations - concat
+""" 1. Basic annotations - concat
 """
 
-def concat(str1:str, str2:str) -> str:
-    """
-        Concatenates two strings.
-    """
-    return str(str1) + (str2)
 
-def concat(str1:str, str2:str) -> str:
+def concat(str1: str, str2: str) -> str:
+    """ Concatenates two strings.
     """
-        Concatenates two strings together.
+    return str(str1) + str(str2)
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Concatenates two strings .
     """
     return str(str1) + str(str2)
